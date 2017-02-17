c577051906c27ad71a1fcdc658caccc3387da7f9
Andreas Fink <andreas@fink.org>
2017-02-17 22:32:58 +0100

    initial checkin of empty files so far

---
+++ ulibisup/ulibisup.h
@@ -0,0 +1,19 @@
+//
+//  ulibisup.h
+//  ulibisup
+//
+//  Created by Andreas Fink on 17.02.17.
+//  Copyright © 2017 Andreas Fink. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ulibisup.
+FOUNDATION_EXPORT double ulibisupVersionNumber;
+
+//! Project version string for ulibisup.
+FOUNDATION_EXPORT const unsigned char ulibisupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ulibisup/PublicHeader.h>
+
+
